53c80978f07ec5ba0860bec5cb9072102128bf20
bbrown585 <brandon_scott585@yahoo.com>
2022-10-24 14:53:45 -0700

    setup mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,2 +1,8 @@
-#!/usr/bin/python3
-"""a script that prepares a MySQL server for the project"""
+--a script that prepares a MySQL server for the project
+--The password of hbnb_dev should be set to hbnb_dev_pwd
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,2 +1,1 @@
 #!/usr/bin/python3
-""" a script that prepares a MySQL server for the project"""
