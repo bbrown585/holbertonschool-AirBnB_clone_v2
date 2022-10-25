0c1798e7005562af3a05fb60713ae984550a2ecb
bbrown585 <brandon_scott585@yahoo.com>
2022-10-25 05:34:16 -0700

    setup mysql_test file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+-- a script that prepares a MySQL server for the project
+-- The password of hbnb_dev should be set to hbnb_test_pwd
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO hbnb_test@localhost;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+FLUSH PRIVILEGES;
