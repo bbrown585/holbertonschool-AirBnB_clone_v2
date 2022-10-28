5c85ddb14432e012b52e64ea44a13b374dfb9c14
bbrown585 <brandon_scott585@yahoo.com>
2022-10-27 19:48:41 -0700

    user update

--- models/user.py
+++ models/user.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from models.place import Place
-from models.review import Review
-from sqlalchemy import Column,  Integer, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
@@ -15,9 +13,13 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    places = relationship('Place',
-                          cascade="all, delete, delete-orphan",
-                          backref='user')
-
-    reviews = relationship('Review', cascade="all, delete, delete-orphan",
-                           backref='user')
+    places = relationship(
+        "Place",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
+    reviews = relationship(
+        "Review",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
