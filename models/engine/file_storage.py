757ae54fbf0e71fe09bae980341b52e34b048571
bbrown585 <brandon_scott585@yahoo.com>
2022-10-27 19:31:18 -0700

    working file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-
-
-classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
-           'State': State, 'City': City, 'Amenity': Amenity,
-           'Review': Review}
 
 
 class FileStorage:
@@ -27,6 +15,16 @@ class FileStorage:
         """
         if cls is None:
             return self.__objects
+        class_dict = {}
+        # print("All FileStorage")
+        for key, value in self.__objects.items():
+            _class = value.__class__
+            _value = value.__class__.__name__
+            # _class = cls.__name__
+            # _obj = type(value).__name__
+            if cls == _class or cls == _value:
+                class_dict[key] = value
+        return class_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -43,6 +41,19 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -60,3 +71,7 @@ class FileStorage:
         for key, value in dict(FileStorage.__objects).items():
             if value == obj:
                 del FileStorage.__objects[key]
+    def close(self):
+        """call reload method for deserializing the JSON file to objects
+        """
+        self.reload()
