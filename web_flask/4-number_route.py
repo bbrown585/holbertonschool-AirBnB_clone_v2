03665464ea7e39a45240ee50b725f2ea48183091
bbrown585 <brandon_scott585@yahoo.com>
2022-11-15 19:15:23 -0800

    C is fun

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application and displays an integer
+"""
+from flask import Flask
+from flask import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Returns the string Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Returns the string HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """Returns a string starting with C"""
+    mod_text = escape(text)
+    return 'C %s' % mod_text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text="is cool"):
+    """Returns a string starting with Python"""
+    mod_text = escape(text)
+    return 'Python %s' % mod_text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hello_int(n):
+    """Returns a number"""
+    return '%d is a number' % n
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Starts a Flask web application and displays an integer in HTML
+"""
+from flask import Flask
+from flask import escape
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """Returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """Returns C"""
+    mod_text = escape(text)
+    return 'C %s' % mod_text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text="is cool"):
+    """Returns a string starting with Python"""
+    mod_text = escape(text)
+    return 'Python %s' % mod_text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def hello_int(n):
+    """Returns a number"""
+    return '%d is a number' % n
+
+
+@app.route('/number_template/', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def hello_number_template(n):
+    """Returns an HTML page with a number"""
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
