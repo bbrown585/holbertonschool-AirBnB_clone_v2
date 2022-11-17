37fcf2ed4780d416651466c232965ebaee743703
bbrown585 <brandon_scott585@yahoo.com>
2022-11-16 19:54:15 -0800

    task 8 try

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"a script that starts a Flask web application"
+from flask import Flask, render_template
+from models import storage
+from models import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/states_list")
+def states_list():
+    """
+    show a list of states
+    """
+    all_states = storage.all(State)
+    return render_template('7-states_list.html', all_states=all_states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    close current session
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
